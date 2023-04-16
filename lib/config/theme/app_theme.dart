8eba2002cc11f5d2ee2b28626e8275561b82a796
Emilio Martel <emilio_martel@outlook.es>
2023-04-16 22:07:44 +0100

    app_theme created to custom app theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.pink,
+  Colors.orange,
+  Colors.pinkAccent,
+];
+
+
+
+class AppTheme{
+
+  final int selectedColor;
+
+  const AppTheme({
+    this.selectedColor = 0
+  }): assert( selectedColor >= 0, 'Selected color must be greater than 0' ),
+      assert( selectedColor <= (colorList.length-1), 'Selected color must be less than ${colorList.length}' );
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(centerTitle: false)
+  );
+
+
+
+}
