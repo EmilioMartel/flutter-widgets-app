283fad43cf2fcba7a9a5751b5c66c5298a6966f4
Emilio Martel <emilio_martel@outlook.es>
2023-08-27 19:35:28 +0100

    button screen finish

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -74,7 +74,7 @@ class _ButtonsView extends StatelessWidget {
             ),
 
             //todo:custom button
-
+            const CustomButton(),
 
             IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
             IconButton(
@@ -90,6 +90,32 @@ class _ButtonsView extends StatelessWidget {
 
             
           ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola mundo', style: TextStyle(color: Colors.white),)
+          ),
         ),
       ),
     );
