5f08b5975e0d0f8f698f5200a88c88fff0992fbe
Emilio Martel <emilio_martel@outlook.es>
2023-08-28 18:57:12 +0100

    ui screen finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -44,10 +44,10 @@ const appMenuItems = <MenuItems> [
   ),
 
   MenuItems(
-    title: 'Animated container', 
-    subTitle: 'Stateful widget animado', 
-    link: '/animated', 
-    icon: Icons.check_box_outlined
+    title: 'UI Controls + Tiles', 
+    subTitle: 'Una serie de controles de Flutter', 
+    link: '/ui-controls', 
+    icon: Icons.car_rental_outlined
   ),
 
 
