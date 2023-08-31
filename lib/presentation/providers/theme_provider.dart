bc4961b7206fb1811f19fad3a731e48abba703b3
Emilio Martel <emilio_martel@outlook.es>
2023-08-31 23:57:03 +0100

    themeNotifier finalizado

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -12,3 +12,27 @@ final isDarkModeProvider = StateProvider<bool>((ref) => false);
 
 // Un simple int
 final selectedColorProvider = StateProvider<int>((ref) => 0);
+
+
+// Un objeto de tipo Apptheme (custom)
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+);
+
+
+// Controller o Notifier
+class ThemeNotifier extends StateNotifier<AppTheme>{
+  // state = new AppTheme();
+  ThemeNotifier(): super( AppTheme() );
+
+
+  void toggleDarkMode() {
+    state = state.copyWith( isDarkMode: !state.isDarkMode );
+  }
+
+  void changeColorIndex( int colorIndex ) {
+    state = state.copyWith( selectedColor: colorIndex );
+  }
+  
+
+}
